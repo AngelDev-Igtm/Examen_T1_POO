e1b72c223003201984d0589db8ab452b2e2f9e60
Angel Marin <angeleloymarindiaz2020@gmail.com>
2026-09-21 21:03:47 -0500

    metodo validardni

--- src/AlumnoController.java
+++ src/AlumnoController.java
@@ -3,12 +3,17 @@ import java.util.ArrayList;
 
 public class AlumnoController 
 {
-        public void AgregarAlumno(String AlumnoNuevo) {
+    public void AgregarAlumno(String AlumnoNuevo) {
         if (AlumnoNuevo != null) {
             this.AgregarAlumno(AlumnoNuevo);
             System.out.println("Alumno agregado correctamente.");
         } else {
             System.out.println("Error: No hay nada.");
         }
+    }
+    
+    public void ValidarDni()
+    {
+
     }
 }
