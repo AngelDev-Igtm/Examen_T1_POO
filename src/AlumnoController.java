5d1a0738dac3e404def398b33f952cdf3924db8f
Angel Marin <angeleloymarindiaz2020@gmail.com>
2026-09-21 20:21:28 -0500

    Agregue metodos a la clase controller

--- src/AlumnoController.java
+++ src/AlumnoController.java
@@ -1,3 +1,19 @@
+
+import java.util.ArrayList;
+
 public class AlumnoController {
+    ArrayList<Alumno> alumnos = new ArrayList<>();
     
+    public void codigo()
+    {
+        
+    }
+    public void ValidarDni()
+    {
+        
+    }
+    public void ValidarCarnet()
+    {
+        
+    }
 }
