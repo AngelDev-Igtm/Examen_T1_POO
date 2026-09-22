b279537d8255420ff36b5d0e779ac61d50342800
Angel Marin <angeleloymarindiaz2020@gmail.com>
2026-09-21 19:24:10 -0500

    Metodos privados de alumno creados y constructor

---
+++ src/Alumno.java
@@ -0,0 +1,61 @@
+public class Alumno {
+    private String nombre;
+    private String ApellidoPat;
+    private String ApellidoMat;
+    private String DNI;
+    private String carnet;
+    private String beca;
+
+    public Alumno() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPat() {
+        return ApellidoPat;
+    }
+
+    public void setApellidoPat(String ApellidoPat) {
+        this.ApellidoPat = ApellidoPat;
+    }
+
+    public String getApellidoMat() {
+        return ApellidoMat;
+    }
+
+    public void setApellidoMat(String ApellidoMat) {
+        this.ApellidoMat = ApellidoMat;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+
+    public String getBeca() {
+        return beca;
+    }
+
+    public void setBeca(String beca) {
+        this.beca = beca;
+    }
+    
+    
+}
