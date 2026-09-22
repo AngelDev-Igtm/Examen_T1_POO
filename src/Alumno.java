88811c6469f39b91611ad66f632d56562fe7848a
Angel Marin <angeleloymarindiaz2020@gmail.com>
2026-09-21 20:58:37 -0500

    correcciones 2

--- src/Alumno.java
+++ src/Alumno.java
@@ -78,8 +78,9 @@ public class Alumno {
     public String VerDatos()
     {
         System.out.println("Codigo de Alumno: " + this.codigo + "Nombre del Alumno: " + this.nombre + "Apellido Paterno del Alumno: " 
-                            + this.ApellidoPat + "Apellido Materno del Alumno: " + this.ApellidoMat + "Ingrese el DNI del ALumno: " + this.DNI
-                            + "ingrese el carnet del alumno SI/NO: " +this.carnet + "El Alumno cuenta con beca?: " + this.beca);
+                            + this.ApellidoPat + "Apellido Materno del Alumno: " + this.ApellidoMat + "Ingrese el DNI del ALumno: " + this.DNI 
+                            +"Ingrese el nivel socieconomico: "+ this.Nivelsocieconomico + "ingrese el carnet del alumno SI/NO: " +this.carnet + 
+                            "El Alumno cuenta con beca?: " + this.beca);
         return VerDatos();
     }
 }
