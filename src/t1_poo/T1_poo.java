1b8ba9e61f127fe122ad284bc0c33e9ad3ece058
Angel Marin <angeleloymarindiaz2020@gmail.com>
2026-09-21 19:38:37 -0500

    Clase scaner en la vista

--- src/t1_poo/T1_poo.java
+++ src/t1_poo/T1_poo.java
@@ -1,8 +1,35 @@
 package t1_poo;
+import java.util.Scanner;
 
 public class T1_poo {
     
     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese código del alumno: ");
+        String codigo = scanner.nextLine();
+
+        System.out.print("Ingrese nombre del alunmo: ");
+        String nombre = scanner.nextLine();
+
+        System.out.print("Ingrese el apellido paterno: ");
+        String ApellidoPat = scanner.nextLine();
+        
+        System.out.print("Ingrese el apellido materno: ");
+        String ApellidoMat = scanner.nextLine();
+        
+        System.out.print("Ingrese el DNI del alumno: ");
+        String DNI = scanner.nextLine();
+        
+        try {
+            System.out.print("Ingrese el carnet si tuviese: ");
+            String carnet = scanner.nextLine();
+        }catch (Exception e) 
+        {
+            System.out.println("El alumno no cuenta con carnet");
+        }
+        
+        System.out.print("Ingrese el apellido paterno: ");
+        boolean  beca = scanner.hasNextDouble();
     }
-    
 }
